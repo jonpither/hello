c2305a975b2a1e39af62fe61d842d40fb1f5d565
Jon Pither <jonpither@Jons-MacBook-Air.local>
2013-03-12 08:53:54 +0000

    tidied

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.1.8"]]
   :ring {:handler hello.core/handler}
-  :plugins [[lein-ring "0.8.3"]
-;;            [lein-pallet-fuz "0.1.0-SNAPSHOT"]
-            ]
+  :plugins [[lein-ring "0.8.3"]]
+  :profiles {:dep {:plugins [[lein-ring "0.8.3"]
+                             [lein-pallet-fuz "0.1.0-SNAPSHOT"]]}}
   :pallet-fuz {:pub-key-path "/Users/jonpither/.ssh/depl_rsa.pub"
                :pri-key-path "/Users/jonpither/.ssh/depl_rsa"
                :git-url "git@github.com:jonpither/hello.git"
@@ -16,7 +16,6 @@
                :checkout-dir "fuzzage"
                :node-spec {:image {:os-family :ubuntu
                                    :os-version-matches "12.04"}
-                           ;;   :hardware {:min-ram (* 1 1024)}
                            :hardware {:hardware-id "t1.micro"}
                            :network {:inbound-ports [22 3000]}
                            :location {:location-id "us-east-1"}}})
