332565a94cb9913277a007c34e9ae5df1b10cc36
Jon Pither <jonpither@Jons-MacBook-Air.local>
2013-03-13 16:01:12 +0000

    changing plugin deps in profile

--- project.clj
+++ project.clj
@@ -6,16 +6,19 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring "1.1.8"]]
   :ring {:handler hello.core/handler}
-  :plugins [[lein-ring "0.8.3"]]
-  :profiles {:dep {:plugins [[lein-ring "0.8.3"]
-                             [lein-pallet-fuz "0.1.0-SNAPSHOT"]]}}
+  :profiles {:dev {:plugins [[lein-ring "0.8.3"]
+                             [lein-pallet-fuz "0.1.0-SNAPSHOT"]
+                             [org.cloudhoist/pallet-lein "0.5.1"]]}}
   :pallet-fuz {:pub-key-path "/Users/jonpither/.ssh/depl_rsa.pub"
                :pri-key-path "/Users/jonpither/.ssh/depl_rsa"
                :git-url "git@github.com:jonpither/hello.git"
-               :user "fuzzer"
-               :checkout-dir "fuzzage"
+
+               :service-name "bob"
+               :group-name "bobbuilder"
+
+               :port 3000
                :node-spec {:image {:os-family :ubuntu
                                    :os-version-matches "12.04"}
                            :hardware {:hardware-id "t1.micro"}
-                           :network {:inbound-ports [22 3000]}
+                           :network {:inbound-ports [22 80 3000]}
                            :location {:location-id "us-east-1"}}})
