dc3cfe2458493335fdb0162abe65f3387e71cb27
Jon Pither <jonpither@Jons-MacBook-Air.local>
2013-03-24 13:01:46 +0000

    paellet proj file

--- project.clj
+++ project.clj
@@ -1,24 +1,32 @@
 (defproject hello "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+
+  ;; Add a service
+  ;;  lein pallet add-service aws aws-ec2 {identity} {credentials}
+
+  :description "Sample project using pallet and pallet-fuz crate"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+
+  :repositories {"sonatype" "http://oss.sonatype.org/content/repositories/releases"
+                 "sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"}
+
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [ring "1.1.8"]]
+
+  :profiles {:dev {:plugins [[com.palletops/pallet-lein "0.6.0-beta.7"]]}
+             :pallet {:dependencies
+                      ;; The latest pallet
+                      [ [com.palletops/pallet "0.8.0-beta.5"]
+                        [org.cloudhoist/pallet-jclouds "1.5.2"]
+
+                        [org.jclouds.provider/aws-ec2 "1.5.5"]
+                        [org.jclouds.provider/aws-s3 "1.5.5"]
+
+                        [org.jclouds.driver/jclouds-slf4j "1.5.5"]
+                        [org.jclouds.driver/jclouds-jsch "1.5.5"]
+
+                        [lein-pallet-fuz "0.1.2-SNAPSHOT"]]}}
+
   :ring {:handler hello.core/handler}
-  :plugins [[lein-ring "0.8.3"]]
-  :profiles { :dep {:plugins [[lein-pallet-fuz "0.1.0-SNAPSHOT"]
-                              [org.cloudhoist/pallet-lein "0.5.1"]]}}
-  :pallet-fuz {:pub-key-path "/Users/jonpither/.ssh/depl_rsa.pub"
-               :pri-key-path "/Users/jonpither/.ssh/depl_rsa"
-               :git-url "git@github.com:jonpither/hello.git"
-
-               :service-name "bob"
-               :group-name "bobbuilder"
-
-               :port 3000
-               :node-spec {:image {:os-family :ubuntu
-                                   :os-version-matches "12.04"}
-                           :hardware {:hardware-id "t1.micro"}
-                           :network {:inbound-ports [22 80 3000]}
-                           :location {:location-id "us-east-1"}}})
+  :plugins [[lein-ring "0.8.3"]])
